ea63ab9a9c554452a787f30e058e6e16a88fe954
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-06 08:43:51 +0530

    yes me

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useReducer } from 'react';
+const reducerFunction = (state, action) => { };
+const initialState = {
+  count: 0,
+};
 function App() {
+  const [input, setInput] = useState(0);
+  const [state, dispatch] = useReducer(reducerFunction, initialState);
   return (
     <div className="App">
-     
+
     </div>
   );
 }
