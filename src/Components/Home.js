ef6893cb86b081d9a2612e9b28a9cee81ac4a904
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-14 12:40:54 +0530

    yes me

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,12 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import AppContext from '../Context/app-context';
-function Home(){
-  const {message1 } = useContext(AppContext);
- return(
-     <>
-       <h1>This is Home Component</h1>
-       <p>{message1}</p>
-     </>
-)}
+function Home() {
+  const { message1, setMessage1 } = useContext(AppContext);
+  return (
+    <>
+      <h1>This is Home Component</h1>
+      <p>{message1}</p>
+    </>
+  )
+}
 export default Home;
--- src/Context/AppState.js
+++ src/Context/AppState.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import AppContext from './app-context';
 function AppState(props) {
+    const [message1, setMessage1] = useState("Message1");
+    const [message2, setMessage2] = useState("Message2");
     return (
         <>
-            <AppContext.Provider value={{ message1: "This is a message for home page", message2: "This is message for details" }}>
+            <AppContext.Provider value={{ message1: message1, setMessage1, setMessage2, message2: message2 }}>
                 {props.children}
             </AppContext.Provider>
         </>
