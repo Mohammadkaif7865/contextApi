69a521e977f4b77c1bf699837ee75afcd0cdc4db
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-14 12:35:11 +0530

    ues me

--- src/Context/AppState.js
+++ src/Context/AppState.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppContext from './app-context';
 function AppState(props) {
     return (
--- src/Context/app-context.js
+++ src/Context/app-context.js
@@ -1,3 +1,3 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext } from 'react';
 const appContext = createContext();
 export default appContext;
