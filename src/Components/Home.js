5940e8e755c0deb339b0329bfe810fa2cc50dc6f
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-16 13:23:53 +0530

    yes em

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import AppContext from '../Context/app-context';
 function Home() {
-  const { message1, isAuth, setIsAuth } = useContext(AppContext);
+  const { message1, isAuth, setIsAuth ,setMessage1} = useContext(AppContext);
   return (
     <>
       <h1 style={{ backgroundColor: isAuth ? "green" : "red" }}>This is Home Component</h1>
--- src/Context/AppState.js
+++ src/Context/AppState.js
@@ -6,7 +6,7 @@ function AppState(props) {
     const [isAuth, setIsAuth] = useState(true);
     return (
         <>
-            <AppContext.Provider value={{ setIsAuth, message1, isAuth, message2 }}>
+            <AppContext.Provider value={{ setIsAuth, setMessage1, setMessage2, message1, isAuth, message2 }}>
                 {props.children}
             </AppContext.Provider>
         </>
