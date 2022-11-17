aff71399f018cacdafe19298b5ce3b87ffc3cc9e
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-17 20:03:21 +0530

    yes me

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext,useState } from 'react';
 import AppContext from '../Context/app-context';
 function Detail() {
     const { message2, setMessage2, isAuth, setIsAuth } = useContext(AppContext);
+    const [message, setMessage] = useState(message2);
     return (
         <>
             <h1 style={{ backgroundColor: isAuth ? 'green' : 'red' }} >This is the detail component</h1>
             {
                 isAuth ? <button onClick={() => setIsAuth(false)}>Logout</button> : <button onClick={() => setIsAuth(true)}>Login</button>
             }
+            <form >
+                <label htmlFor="message">Message2</label>
+                <input type="text" id="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+                <button onClick={() => setMessage2(message)}></button>
+            </form>
             <p>{message2}</p>
         </>
     )
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AppContext from '../Context/app-context';
 function Home() {
-  const { message1, isAuth, setIsAuth ,setMessage1} = useContext(AppContext);
+  const { message1, isAuth, setIsAuth, setMessage1 } = useContext(AppContext);
+  const [message, setMessage] = useState(message1);
   return (
     <>
       <h1 style={{ backgroundColor: isAuth ? "green" : "red" }}>This is Home Component</h1>
       {
         isAuth ? <button onClick={() => setIsAuth(false)}>Logout</button> : <button onClick={() => setIsAuth(true)}>Login</button>
       }
+      <form >
+        <label htmlFor="message">Message1</label>
+        <input type="text" id="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={() => setMessage1(message)}></button>
+      </form>
       <p>{message1}</p>
     </>
   )
