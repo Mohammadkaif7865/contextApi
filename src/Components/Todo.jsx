b4552e6399d405dba708bbdc990154f2d049da89
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-19 08:37:32 +0530

    .jsx extension is best

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import AppState from '../Context/AppState';
 function Todo() {
     return (
         <>
             <h1>This is TODO page</h1>
             <label htmlFor="todo">TODO</label>
             <input id='todo' type="text" />
-             
+
         </>
     )
 }
