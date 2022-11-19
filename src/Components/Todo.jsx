d04494641fd8717136d78ac150eafb94ad8edbb7
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-19 08:28:15 +0530

    this one is new

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,9 +3,9 @@ function Todo() {
     return (
         <>
             <h1>This is TODO page</h1>
-            <a aria-label="Chat on WhatsApp" href="https://wa.me/+918800588492"> <img alt="Chat on WhatsApp" src="WhatsAppButtonGreenLarge.png" />
-            </a>
-
+            <label htmlFor="todo">TODO</label>
+            <input id='todo' type="text" />
+             
         </>
     )
 }
