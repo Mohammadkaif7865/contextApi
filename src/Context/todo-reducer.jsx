5c7c0e6d8e7b8005e4212aff0358230ecceddb0b
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-21 07:10:53 +0530

    yes me

--- src/Context/todo-reducer.jsx
+++ src/Context/todo-reducer.jsx
@@ -1,10 +1,14 @@
+import { ADD_TODO, DELETE_TODO, TOGGLE_TODO } from "./todo-action";
 const todoReducer = (state, action) => {
     switch (action.type) {
-        case value:
-
-            break;
+        case ADD_TODO:
+       return {}
+        case DELETE_TODO:
+       return {}
+        case TOGGLE_TODO:
+       return {}
 
         default:
-            break;
+            return state;
     }
 }
