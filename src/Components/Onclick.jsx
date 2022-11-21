3b366850618b7fb9a33132183184705d6ecc6fba
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-21 20:22:46 +0530

    higher order components

---
+++ src/Components/Onclick.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+function Onclick() {
+    const [counter, setCounter] = useState(0);
+    const increamentCounter = () => setCounter(counter + 1);
+    return (
+        <>
+            <div>
+                <button className='btn btn-light' onClick={() => increamentCounter()}>Clicked {counter} number of times</button>
+            </div>
+        </>
+    )
+}
+export default Onclick;
