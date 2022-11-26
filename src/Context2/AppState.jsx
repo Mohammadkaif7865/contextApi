f33be44bc6949940e549993f5461645f2d6ef53d
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-26 18:37:20 +0530

    this is reducer tobe done

--- src/Context2/AppState.jsx
+++ src/Context2/AppState.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import AppContext from '../Context/app-context';
+import { DELETE_STUDENT, ADD_STUDENT, FEE_PAID } from './student-action';
 import reducerFunction from './student-reducer';
 function AppState(props) {
+    const initialState = {
+        students: [{ name: "Rohan", schoolRollNumber: "12122008rohan", class: 2, section: 'A', feesPaid: false }]
+    }
+    const [state, dispatch] = useReducer(reducerFunction, initialState);
+    // * FeePaid 
+
     return (
         <>
             <AppContext.Provider>
