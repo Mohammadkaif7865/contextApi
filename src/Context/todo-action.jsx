0af2fa76d9555a5a11171e034273ac086d564c49
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-28 08:15:23 +0530

    only one reducer function and context is allowed

--- src/Context/todo-action.jsx
+++ src/Context/todo-action.jsx
@@ -1,3 +1,6 @@
 export const ADD_TODO = "ADD_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 export const DELETE_TODO = "DELETE_TODO";
+export const DELETE_STUDENT = "DELETE_STUDENT";
+export const ADD_STUDENT = "ADD_STUDENT";
+export const FEE_PAID = "FEE_PAID";
--- src/Context2/student-action.jsx
+++
@@ -1,3 +0,0 @@
-export const DELETE_STUDENT = "DELETE_STUDENT";
-export const ADD_STUDENT = "ADD_STUDENT";
-export const FEE_PAID = "FEE_PAID";
