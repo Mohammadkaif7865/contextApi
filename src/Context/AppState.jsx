4588188715b20948f758e9df24b12cd7922dcdc3
mohammad kaif <mohammadkaif051197@gmail.com>
2022-11-30 12:38:01 +0530

    yes em

--- src/Context/AppState.jsx
+++ src/Context/AppState.jsx
@@ -1,11 +1,16 @@
-import React, { useState, useReducer } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import AppContext from './app-context';
 import reducerFunction from './todo-reducer';
 import { ADD_TODO, TOGGLE_TODO, ADD_STUDENT, DELETE_STUDENT, FEE_PAID, DELETE_TODO } from './todo-action';
+const url = "https://energetic-cyan-fedora.cyclic.app/getStudents";
 function AppState(props) {
+    const [students, setStudents] = useState([]);
+    useEffect(() => {
+        fetch(url, { method: 'GET' }).then((response) => response.json()).then((data) => setStudents(data));
+    }, [])
     const initialState = {
         todos: [{ id: 43423, text: 'make coffee', complete: false }], // # {id: random_number , text: random_string , complete: booleans}
-        students: [{ name: "Rohan", schoolCode: "12122008ROHAN", dob: '20-2-2013', standard: 2, section: 'A', feesPaid: false }]
+        students: students
     }
     const [state, dispatch] = useReducer(reducerFunction, initialState);
     const [message1, setMessage1] = useState("Message1");
