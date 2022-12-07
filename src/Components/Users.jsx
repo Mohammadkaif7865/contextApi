d97caf93fbe6d62e62d49505e90c80f540f9ca21
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-07 08:20:45 +0530

    yes em

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
-function Users(){
- return(
-     <>
-       <h1>This is Users component</h1>
-     </>
-)}
+import React, { useEffect, useState } from 'react';
+function Users() {
+    const url = 'https://energetic-cyan-fedora.cyclic.app/users';
+    let [users, setUsers] = useState('');
+    useEffect(() => {
+        fetch(url, { method: 'GET' }).then((response) => response.json()).then((data) => setUsers(data));
+    }, []);
+    console.log(users);
+    return (
+        <>
+            <h1>This is Users component</h1>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">UserId</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Website</th>
+                        <th scope="col">Company</th>
+                        <th scope="col">Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                      users?   users.map((user, i) =>
+                      <tr key={i}>
+                          <th scope="row">{user.id}</th>
+                          <td>{user.name}</td>
+                          <td>{user.email}</td>
+                          <td>{user.phone}</td>
+                          <td>{user.website}</td>
+                          <td>{user.company.name}</td>
+                          <td>{user.address.street}</td>
+                      </tr>):   
+                            <tr>
+                                <th scope="row">----------</th>
+                                <td>----------</td>
+                                <td>----------</td>
+                                <td>----------</td>
+                                <td>----------</td>
+                                <td>----------</td>
+                                <td>----------</td>
+                            </tr>
+                    }
+
+                </tbody>
+            </table>
+        </>
+    )
+}
 export default Users;
