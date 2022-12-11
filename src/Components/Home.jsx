a815e6189929bd6cac83ed8d39e90dab2ddc7459
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-11 07:14:45 +0530

    yes me

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import AppContext from '../Context/app-context';
 function Home() {
-  // just taking back date commits for 09/12/2022 and 10/12/2022
+  // just taking back date commits for 09/12/2022 and 10/12/2022 and now onwards
   const { message1, isAuth, setIsAuth, setMessage1 } = useContext(AppContext);
   const [message, setMessage] = useState(message1);
   return (
