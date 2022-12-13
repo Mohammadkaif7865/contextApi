dc5cd0287135141f0af766543169f77240133090
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-13 08:41:42 +0530

    yes me

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Onclick from './Onclick';
 import Hover from './Hover';
+import Item from './Item';
 function Display() {
     return (
         <>
             <Onclick />
             <Hover />
+            <Item />
         </>
     )
 }
--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import Wrapper2 from './Wrapper2';
 function Item(props) {
-    console.log(props);
+    const [value, setValue] = useState('');
     return (
         <>
-            <h1>This is item component</h1>
+            <h1>Name: {props.name}</h1>
+            <input type="text" value={value} onChange={(e) => { props.setName(e.target.value); setValue(e.target.value) }} />
         </>
     )
 }
