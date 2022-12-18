11c91e6afc89ad96a1621e798fe54e8bf5fc962c
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-18 09:02:09 +0530

    yes me

--- src/Components/Refdemo.jsx
+++ src/Components/Refdemo.jsx
@@ -2,19 +2,30 @@ import React, { useRef, useEffect } from 'react';
 function Ref() {
     const inputRef = useRef();
     const handleSubmit = () => {
-         alert(` input value is ${inputRef.current.value}`);
+        if (refer) {
+            console.log(refer.value);
+        }
+        // alert(` input value is ${inputRef.current.value}`);
+    }
+    let refer = null;
+    const setRef = (e) => {
+        refer = e;
     }
     useEffect(() => {
-        inputRef.current.focus();
+        if (refer) {
+            refer.focus();
+        }
     }, [])
     return (
         <>
             <div className="container">
                 <h1>This is Ref Component</h1>
                 <input type="text" ref={inputRef} />
+                <input type="text" ref={setRef} />
                 <button onClick={handleSubmit}>Submit</button>
             </div>
         </>
     )
 }
 export default Ref;
+// ? We do not always require hooks for ref we can simply pass element with its dom object in a function so for our easiness react have created useRef
