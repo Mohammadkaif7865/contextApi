9f9d80bbfb3009d52b960949fe66c1b8bfe2fd72
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-18 08:36:15 +0530

    yes em

--- src/Components/Refdemo.jsx
+++ src/Components/Refdemo.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-function Ref(){
- return(
-     <>
-      <h1>This is the ref components</h1> 
-     </>
-)}
+import React, { useRef, useEffect } from 'react';
+function Ref() {
+    const inputRef = useRef();
+    useEffect(() => {
+        console.log(inputRef);
+    }, [])
+    return (
+        <>
+            <div className="container">
+                <h1>This is Ref Component</h1>
+                <input type="text" ref={inputRef} />
+            </div>
+        </>
+    )
+}
 export default Ref;
