f284474bbca347e020e2a9a1cb38c29140dd8c4c
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-18 08:40:24 +0530

    yes me

--- src/Components/Refdemo.jsx
+++ src/Components/Refdemo.jsx
@@ -1,14 +1,18 @@
 import React, { useRef, useEffect } from 'react';
 function Ref() {
     const inputRef = useRef();
+    const handleSubmit = () => {
+         alert(` input value is ${inputRef.current.value}`);
+    }
     useEffect(() => {
-        console.log(inputRef);
+        inputRef.current.focus();
     }, [])
     return (
         <>
             <div className="container">
                 <h1>This is Ref Component</h1>
                 <input type="text" ref={inputRef} />
+                <button onClick={handleSubmit}>Submit</button>
             </div>
         </>
     )
