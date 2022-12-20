b94654f10691221e6c291c65395d9ecf3219ba1e
mohammad kaif <mohammadkaif051197@gmail.com>
2022-12-20 07:51:12 +0530

    yes me

---
+++ src/Components/FocusInput.jsx
@@ -0,0 +1,13 @@
+import React, {useRef,useEffect} from 'react';
+import Input from './Elements/Input';
+function FocusInput(){
+    let inputRef = useRef();
+    useEffect(()=>{
+        console.log(inputRef);
+    },[])
+ return(
+     <>
+       <Input ref={inputRef}/>
+     </>
+)}
+export default FocusInput;
